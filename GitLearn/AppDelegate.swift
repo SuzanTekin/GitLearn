fcd10b4f324f49fb2c64cd463860c3ae84ed418a
Suzan Tekin <suzanpisat@gmail.com>
2022-06-07 13:45:47 +0300

    asdf

--- GitLearn/AppDelegate.swift
+++ GitLearn/AppDelegate.swift
@@ -10,7 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//asdA
+//gggg
+    //aasd
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
